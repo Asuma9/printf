cdba145a73db0381afbe747c3a0f37d06b1a8a5b
Desmond <karanidesmond@protonmail.ch>
2023-08-22 09:56:08 +0300

    A code module for custom printf

--- Alternative/main.h
+++ Alternative/main.h
@@ -4,12 +4,12 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+int _putchar(char c);
+int my_putchar(int a, int counter);
 int _printf(const char *format, ...);
-int my_putchar(int a, int count);
 int (*selector(char c))(va_list);
-int _char(va_list args);
-int _string(va_list args);
-int _decimal(va_list args);
-int _percentage(va_list args);
+int _char(va_list arg);
+int _decimal(va_list arg);
+int _string(va_list arg);
 
-#endif /* MAIN_H */
+#endif
